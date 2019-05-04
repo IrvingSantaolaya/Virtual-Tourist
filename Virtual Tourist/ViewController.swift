04778e0ea0a26d293ae7553c9aedf6f97ea1bce1
Irving M Santaolaya <irvingcodes@gmail.com>
2019-05-03 21:28:29 -0700

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Irving Martinez on 5/3/19.
+//  Copyright © 2019 Irving Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
